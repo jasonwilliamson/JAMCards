267570b56d1ff294a3303e2e7f1122aa68479248
mygu <mygu@uwaterloo.ca>
2018-07-16 16:14:42 -0400

    Making DeckQuizStats retrieve data from database

--- JAMMCards_ver1/app/src/main/java/com/co/jammcards/jammcards/DeckQuizStatsActivity.java
+++ JAMMCards_ver1/app/src/main/java/com/co/jammcards/jammcards/DeckQuizStatsActivity.java
@@ -12,10 +12,14 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
+import java.util.UUID;
+import java.util.List;
 
 public class DeckQuizStatsActivity extends AppCompatActivity {
 
-    PieChart pieChart;
+    private PieChart pieChart;
+    private Deck mDeck;
+    private List<Card> mCards;
 
     public static Intent newIntent(Context packageContent) {
         Intent intent = new Intent(packageContent, DeckQuizStatsActivity.class);
@@ -27,56 +31,95 @@ public class DeckQuizStatsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deck_quiz_stats);
 
-        pieChart = findViewById(R.id.quizPieChart);
-        pieChart.setRotationEnabled(true);
-        pieChart.setHoleRadius(10f);
-        pieChart.setDrawEntryLabels(true);
-        pieChart.setEntryLabelTextSize(15);
-        pieChart.setEntryLabelColor(Color.BLACK);
+        // retrieve deck ID
+        UUID deckId = (UUID) getIntent()
+                .getSerializableExtra(CardListActivity.EXTRA_DECK_ID);
+        mDeck = DeckLab.get(this).getDeck(deckId);
+        mCards = CardLab.get(this).getCards(deckId);
 
-        // hardcoded prototype demo data begins here
-        ArrayList<PieEntry> pieSlices = new ArrayList<>();
-        ArrayList<String> pieLabels = new ArrayList<>();
-        ArrayList<Integer> colors = new ArrayList<>();
+        // remove this later
+        //initDummyChartData();
 
-        for (int i = 0; i < 5; ++i)
-        {
-            String letterGrade = "";
+        createPieChart();
+    }
 
-            switch (i) {
-                case 0:
-                    letterGrade = "<50% Correct";
-                    break;
-                case 1:
-                    letterGrade = "50-59% Correct";
-                    break;
-                case 2:
-                    letterGrade = "60-69% Correct";
-                    break;
-                case 3:
-                    letterGrade = "70-79% Correct";
-                    break;
-                case 4:
-                    letterGrade = "80-100% Correct";
-                    break;
+    private void initDummyChartData()
+    {
+        mDeck.setCategoryA(6);
+        mDeck.setCategoryB(9);
+        mDeck.setCategoryC(3);
+        mDeck.setCategoryD(0);
+        mDeck.setCategoryF(0);
+    }
+
+    private void createPieChart()
+    {
+        float totalQuizRuns = mDeck.getCategoryA() + mDeck.getCategoryB() + mDeck.getCategoryC() + mDeck.getCategoryD() + mDeck.getCategoryF();
+
+        // only initialize chart if quiz data has been recorded
+        if (totalQuizRuns > 0) {
+            // styling
+            pieChart = findViewById(R.id.quizPieChart);
+            pieChart.setRotationEnabled(true);
+            pieChart.setHoleRadius(0f);
+            pieChart.setDrawEntryLabels(true);
+            pieChart.setEntryLabelTextSize(15);
+            pieChart.setEntryLabelColor(Color.BLACK);
+
+            // retrieve chart data from deck
+            ArrayList<PieEntry> pieSlices = new ArrayList<>();
+            ArrayList<String> pieLabels = new ArrayList<>();
+            ArrayList<Integer> colors = new ArrayList<>();
+
+
+            // A range
+            float aRuns = ((float)mDeck.getCategoryA()/totalQuizRuns) * 100;
+            pieSlices.add(new PieEntry(aRuns, "80-100% Correct", mDeck.getCategoryA()));
+            if (aRuns > 0) {
+                pieLabels.add("80-100% Correct");
             }
 
-            pieSlices.add(new PieEntry(20f, letterGrade, 5));
-            pieLabels.add(letterGrade);
+            // B range
+            float bRuns = ((float)mDeck.getCategoryB()/totalQuizRuns) * 100;
+            pieSlices.add(new PieEntry(bRuns, "70-79% Correct", mDeck.getCategoryB()));
+            if (bRuns > 0) {
+                pieLabels.add("70-79% Correct");
+            }
+
+            // C range
+            float cRuns = ((float)mDeck.getCategoryC()/totalQuizRuns) * 100;
+            pieSlices.add(new PieEntry(cRuns, "60-69% Correct", mDeck.getCategoryC()));
+            if (cRuns > 0) {
+                pieLabels.add("60-69% Correct");
+            }
+
+            // D range
+            float dRuns = ((float)mDeck.getCategoryD()/totalQuizRuns) * 100;
+            pieSlices.add(new PieEntry(dRuns, "50-59% Correct", mDeck.getCategoryD()));
+            if (dRuns > 0) {
+                pieLabels.add("50-59% Correct");
+            }
+
+            // F range
+            float fRuns = ((float)mDeck.getCategoryF()/totalQuizRuns) * 100;
+            pieSlices.add(new PieEntry(fRuns, "<50% Correct", mDeck.getCategoryF()));
+            if (fRuns > 0) {
+                pieLabels.add("<50% Correct");
+            }
+
+            colors.add(Color.GREEN);
+            colors.add(Color.BLUE);
+            colors.add(Color.YELLOW);
+            colors.add(Color.MAGENTA);
+            colors.add(Color.RED);
+
+            PieDataSet quizDataSet = new PieDataSet(pieSlices, "Quiz Grades");
+            quizDataSet.setColors(colors);
+
+            PieData quizData = new PieData(quizDataSet);
+            pieChart.setData(quizData);
+            pieChart.getLegend().setEnabled(false);
+            pieChart.invalidate();
         }
-
-        colors.add(Color.BLUE);
-        colors.add(Color.RED);
-        colors.add(Color.GREEN);
-        colors.add(Color.YELLOW);
-        colors.add(Color.CYAN);
-
-        PieDataSet quizDataSet = new PieDataSet(pieSlices, "Quiz Grades");
-        quizDataSet.setColors(colors);
-
-        PieData quizData = new PieData(quizDataSet);
-        pieChart.setData(quizData);
-        pieChart.getLegend().setEnabled(false);
-        pieChart.invalidate();
     }
 }
